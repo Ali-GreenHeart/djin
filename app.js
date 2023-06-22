00815eeb6f5886926f02b17e9913f03ed77aa9df
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-06-22 16:15:00 +0400

    add ali and music

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
+const djin = document.querySelector('.djin')
+const fetched = document.querySelector('.fetched')
+const answer = document.getElementById('answer')
+const image = document.getElementById('image')
+const audio = document.querySelector('audio')
 
+let mesaj = ''
+let djinCalled = false
+
+
+window.addEventListener("keydown", (e) => {
+    mesaj += e.key
+    if (mesaj === 'djin') {
+        djin.style.display = 'block'
+        mesaj = ''
+        djinCalled = true
+    } else if (mesaj === 'murad') {
+        if (djinCalled) {
+            fetched.style.display = 'block';
+            answer.textContent = 'Buyur, bu da Murad abi'
+            image.src = "./murad.png"
+        } else {
+            alert('Birinci djin-i cagir')
+        }
+        mesaj = ''
+    } else if (mesaj === 'ali') {
+        if (djinCalled) {
+            fetched.style.display = 'block'
+            answer.textContent = 'Neynirsene alini, al!'
+            image.src = "./lion.jpg"
+        } else {
+            alert('Birinci djin-i cagir')
+        }
+        mesaj = ''
+    } else if (mesaj === 'music') {
+        if (djinCalled) {
+            audio.style.display = 'block'
+            audio.src = './qruz.mp3'
+        } else {
+            alert('Birinci djin-i cagir')
+        }
+        mesaj = ''
+    }
+    console.log(mesaj)
+})
+
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        mesaj = ''
+    }
+})
